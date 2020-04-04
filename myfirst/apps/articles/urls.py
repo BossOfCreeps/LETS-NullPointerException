2aaa2199bca17cd1e90ab7174a5cea1ccfac9953
BossOfCreeps <seva-ra4is@mail.ru>
2020-04-04 09:00:13 +0700

    add auth

--- myfirst/apps/articles/urls.py
+++ myfirst/apps/articles/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+import django
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
-app_name = "articles"
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('events/', views.events, name = 'events'),
-
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
